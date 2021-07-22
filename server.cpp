e27c0d36160db544eecc3ae3bb70d8137e654228
lepecoder <lepecoder@qq.com>
2021-07-22 17:43:29 +0800

    完成第一版，可以接受多个客户端连接，按顺序返回请求，客户端只能发送一次请求

--- server.cpp
+++ server.cpp
@@ -11,7 +11,7 @@
 #define BUFFSIZE 2048
 // #define DEFAULT_PORT 12343
 #define MAXLINK 2048
-int sockfd, connfd; // 定义服务端套接字和客户端套接字
+int sockfd, connfd; // 分别用于监听和通讯
 uint32_t DEFAULT_PORT = 12343;
 void stopServerRunning(int p) {
     printf("int p=%d, SIGINT=2    ", p);
@@ -26,17 +26,17 @@ int main(int argc, char *arg[]) {
         printf("%s\n", arg[1]);
         DEFAULT_PORT = atoi((arg[1]));
     }
-    void (*stopruning)(int p);
+    void (*stopruning)(int p); // 创建函数指针
     stopruning = stopServerRunning;
-    struct sockaddr_in servaddr; // 用于存放IP和端口的结构
-    char buff[BUFFSIZE];         // 用于收发数据
-    // 1. 建立socket连接
+    /* 1.建立socket连接 */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (-1 == sockfd) {
-        printf("Create socket error(%d): %s\n", errno, strerror(errno));
+        // printf("Create socket error(%d): %s\n", errno, strerror(errno));
+        perror("create socket error");
         return -1;
     }
-    // 2. bind网络地址
+    /* 2.bind网络地址 */
+    struct sockaddr_in servaddr;        // 用于存放IP和端口的结构
     bzero(&servaddr, sizeof(servaddr)); // 初始化置零
     servaddr.sin_family = AF_INET;
     // host to net long   主机字节序转网络字节序 32位整型
@@ -47,18 +47,21 @@ int main(int argc, char *arg[]) {
     servaddr.sin_port = htons(DEFAULT_PORT);
     if (-1 == bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
         // printf("Bind error (%d): %s\n", errno, strerror(errno));
-        perror("ERROR");
+        perror("bind error");
         return -1;
     }
-    // 3.监听端口
-    if (-1 == listen(sockfd, MAXLINK)) {
-        printf("Listen error(%d): %s\n", errno, strerror(errno));
+    /* 3.监听端口 */
+    if (-1 == listen(sockfd, MAXLINK)) { // MAXLINK最大连接数量,之后的连接会排队等待
+        // printf("Listen error(%d): %s\n", errno, strerror(errno));
+        perror("listen error");
         return -1;
     }
+    char buff[BUFFSIZE]; // 用于收发数据
     printf("Listening...\n");
     while (true) {
         signal(SIGINT, stopruning); //在control-C的时候关闭服务器
-        connfd = accept(sockfd, NULL, NULL);
+        /* 4.等待客户端连接 */
+        connfd = accept(sockfd, NULL, NULL);  // 后面的NULL保存了客户端的地址信息
         if (-1 == connfd) {
             printf("Accept error(%d): %s\n", errno, strerror(errno));
             return -1;
